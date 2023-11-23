e49bfffbc1e23bf669dc054616244fc936b6c3dd
Ray-linux <ranirava52@gmail.com>
2023-11-23 07:11:53 +0530

    changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import router from './router/route.js';
 import connect from './database/conn.js';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 config();
 
 
