d2b4e474b3364a7087b3d0fba6301f764202cb70
Ray-linux <ranirava52@gmail.com>
2023-11-23 07:08:09 +0530

    first commit

---
+++ database/data.js
@@ -0,0 +1,507 @@
+export default [
+    {
+        id: 1,
+        question: ". Look at this series: 7, 10, 8, 11, 9, 12, ... What number should come next?",
+        option: [
+            '7',
+            '10',
+            '12',
+            '13'
+        ]
+    },
+    {
+        id: 2,
+        question: "Fill in the blanks ELFA, GLHA, ILJA, _____, MLNA",
+        option: [
+            'OLPA',
+            'KLMA',
+            'LLMA',
+            'KLLA'
+        ]
+    },
+    {
+        id: 3,
+        question: "Here are some words translated from an artificial language. 'moolokarn means blue sky' , 'wilkospadi means bicycle race',  'moolowilko means blue bicycle' , Which word could mean racecar?",
+        option: [
+            'wilkozwet',
+            'spadiwilko',
+            'moolobreil',
+            'spadivolo'
+        ]
+    },
+    {
+        id: 4,
+        question: "Tanya is older than Eric. Cliff is older than Tanya. Eric is older than Cliff. If the first two statements are true, the third statement is",
+        option: [
+            'true',
+            'false',
+            'Uncertain',
+            'Some more information required'
+        ]
+    },
+    {
+        id: 5,
+        question: "Which word does NOT belong with the others?",
+        option: [
+            'index',
+            'glossary',
+            'chapter',
+            'book'
+        ]
+    },
+    {
+        id: 6,
+        question: "CUP : LIP :: BIRD : ?",
+        option: [
+            'GRASS',
+            'FOREST',
+            'BEAK',
+            'BUSH'
+        ]
+    },
+    {
+        id: 7,
+        question: "Statement: Medicine ‘P’ is a drug which is causing ripples in the medical field.Assumptions: I. No other drug is causing ripples in the medical field. II. Medicine ‘P’ is a great drug",
+        option: [
+            'If only assumption I is implicit',
+            'If only assumption II is implicit',
+            'if either I or II is implicit',
+            'if neither I or II is implicit'
+        ]
+    },
+    {
+        id: 8,
+        question: "Alphanumeric Series given as: W % ^ K V P 1 I 7 E 0 & 2 9 A F Z N 4 * @ U ? M . How many numbers in the series are preceded by a vowel?",
+        option: [
+            'Four',
+            'one',
+            'Two',
+            'One'
+        ]
+    },
+    {
+        id: 9,
+        question: "From a certain point, Smriti walks 70 m towards the south. Then, she turns to her right & starts walking straight for another 70 m. Then, again turning to her left he walks for 60 m. She then turns to her left & walks for 70 m. How far is she from the starting point?",
+        option: [
+            '120 m',
+            '135 m',
+            '140 m',
+            '130 m'
+        ]
+    },
+    {
+        id: 10,
+        question: "If NOIDA is written as OPJEB, then what will be the code for DELHI?",
+        option: [
+            'EFMAK',
+            'EFAMK',
+            'EFMIJ',
+            'EFMIK'
+        ]
+    },
+    {
+        id: 11,
+        question: "In a box containing twelve balls, some are red, and some are white. The ratio of red to white color balls could be any of the following EXCEPT",
+        option: [
+            '1:1',
+            '1:3',
+            '1:4',
+            '1:2'
+        ]
+    },
+    {
+        id: 12,
+        question: "In a box containing 220 packets, there are both 1kg packets and 2kg packets. The total weight of the box is 300kg. How many 2kg packets are present?",
+        option: [
+            '100',
+            '150',
+            '80',
+            '90'
+        ]
+    },
+    {
+        id: 13,
+        question: 'The valid conclusion based on the given statements "All humans are mortal" and "Socrates is a human" is:',
+        option: [
+            'Socrates is mortal.',
+            'All humans are Socrates.',
+            'Mortal beings are not human.',
+            'Socrates is not mortal.'
+        ]
+    },
+    {
+        id: 14,
+        question: 'In a logic puzzle, if Alice is not taller than Bob, and Bob is not shorter than Carol, who is the tallest among the three?',
+        option: [
+            'Alice',
+            'Insufficient information',
+            'Bob',
+            'Carol'
+        ]
+    },
+    {
+        id: 15,
+        question: 'If x is the average (arithmetic mean) of m and 9, y is the average of 2m and 15, and z is the average of 3m and 18, what is the average of x, y, and z in terms of m?',
+        option: [
+            'm+7',
+            'm+6',
+            '2m+14',
+            '3m+21'
+        ]
+    },
+    {
+        id: 16,
+        question: 'In a group of 100 people, 52 like mango and 68 like apple, and each person likes at least one of the two fruits. How many like both mango and apple?',
+        option: [
+            '52',
+            '48',
+            '20',
+            '68'
+        ]
+    },
+    {
+        id: 17,
+        question: 'Out of all the 2-digit integers between 1 and 100, a 2-digit number has to be selected at random. What is the probability that the selected number is not divisible by 7?',
+        option: [
+            '13/90',
+            '12/90',
+            '77/90',
+            '78/90'
+        ]
+    },
+    {
+        id: 18,
+        question: 'Which one is NOT divisible by 3 ?',
+        option: [
+            '5271',
+            '6291',
+            '4372',
+            '8952'
+        ]
+    },
+    {
+        id: 19,
+        question: 'Sum of the age of 4 children born at interval of 4 years is 36. What is the age of youngest child?',
+        option: [
+            '2 years',
+            '4 years',
+            '5 years',
+            '3 years'
+        ]
+    },
+    {
+        id: 20,
+        question: 'If cos A = tan B, cos B = tan C and cos C = tan A then sin A is equal to',
+        option: [
+            '(√5 - 1) / 2',
+            '(√5 - 1) /4',
+            '(√3 - 1)/4',
+            'None of these'
+        ]
+    },
+    {
+        id: 21,
+        question: 'The maximum value of (sin θ + cos θ) is: ',
+        option: [
+            '1',
+            '√2',
+            '2',
+            '2√2'
+        ]
+    },
+    {
+        id: 22,
+        question: 'if sin θ + cos θ =1, then what is the value of sin θ cos θ',
+        option: [
+            '0',
+            '2',
+            '1',
+            '1/2'
+        ]
+    },
+    {
+        id: 23,
+        question: 'A ranks fourth in a class. B ranks ninth from the last,If C is ninth after A and just in the middle of A and B,How many students are there in the class? ',
+        option: [
+            '33',
+            '32',
+            '31',
+            '30'
+        ]
+    },
+    {
+        id: 24,
+        question: 'Akil ranked seventeenth from the top and thirty seventh from the bottom in a class.How many students are there in the class? ',
+        option: [
+            '53',
+            '45',
+            '54',
+            '52'
+        ]
+    },
+    {
+        id: 25,
+        question: 'Today it is Thursday.After 132 days,it will be ',
+        option: [
+            'Monday',
+            'Sunday',
+            'Wednesday',
+            'Thursday'
+        ]
+    },
+    {
+        id: 26,
+        question: 'If A + B means A is the mother of B; A -B means A is the brother B; A % B means A is the father of B and A x B means A is the sister of B, which of the following shows that P is the maternal uncle of Q?  ',
+        option: [
+            'Q-N + M*P ',
+            'P + S * N-Q',
+            'P -M + N * Q ',
+            'Q-S %P'
+        ]
+    },
+    {
+        id: 27,
+        question: 'Y is in the East of X which is in the North of Z. If P is in the South of Z, then in which direction of Y, is P? ',
+        option: [
+            'North',
+            'South',
+            'South-East',
+            'None of these'
+        ]
+    },
+    {
+        id: 28,
+        question: ' A man walks 5 km toward south and then turns to the right. After walking 3 km he turns to the left and walks 5 km. Now in which direction is he from the starting place? ',
+        option: [
+            'West',
+            'South',
+            'North-East',
+            'South-West'
+        ]
+    },
+    {
+        id: 29,
+        question: 'The ages of Ram and Shyam differ by 16 years. 6 years ago, the Ram was 3 times as old as Shyam. What is the present age of Ram? ',
+        option: [
+            '10 Years',
+            '20 Years',
+            '30 Years',
+            '40 Years'
+        ]
+    },
+    {
+        id: 30,
+        question: 'If 30 % of 1520 + 40 % of 800 =X % of 5000, find the value of x. ',
+        option: [
+            '14.42%',
+            '15.52%',
+            '12.22% ',
+            '18.82% '
+        ]
+    },
+    {
+        id: 31,
+        question: 'A fruit seller had some apples. He sells 40% apples and still has 420 apples. Originally, he had: ',
+        option: [
+            '588 apples',
+            '600 apples ',
+            '672 apples ',
+            '700 apples'
+        ]
+    },
+    {
+        id: 32,
+        question: "What percentage of numbers from 1 to 70 have 1 or 9 in the unit's digit?  ",
+        option: [
+            '1',
+            '14',
+            '20',
+            '21'
+        ]
+    },
+    {
+        id: 33,
+        question: "If selling price is doubled, the profit triples. Find the profit percent.  ",
+        option: [
+            '200/3',
+            '100',
+            '316/3',
+            '120'
+        ]
+    },
+    {
+        id: 34,
+        question: "If 40% of an amount is 250, what will be 60% of that amount? ",
+        option: [
+            '300',
+            '320',
+            '375',
+            '400'
+        ]
+    },
+    {
+        id: 35,
+        question: "In an examination 43% passed in Math, 52% passed in Physics and 52% passed in Chemistry. Only 8% students passed in all the three. 14% passed in Math and Physics and 21% passed in Math and Chemistry and 20% passed in Physics and Chemistry. Number of students who took the exam is 200. Let Set P, Set C and Set M denotes the students who passed in Physics, Chemistry and Math respectively. Then How many students passed in Math only?",
+        option: [
+            '32',
+            '42',
+            '45',
+            '52'
+        ]
+    },
+    {
+        id: 36,
+        question: "In an examination 43% passed in Math, 52% passed in Physics and 52% passed in Chemistry. Only 8% students passed in all the three. 14% passed in Math and Physics and 21% passed in Math and Chemistry and 20% passed in Physics and Chemistry. Number of students who took the exam is 200. Let Set P, Set C and Set M denotes the students who passed in Physics, Chemistry and Math respectively. What is the ratio of the number of students passing in Physics only to the students passing in either Physics or Chemistry or both?",
+        option: [
+            '26:84',
+            '26:8',
+            '2:84',
+            '52:84'
+        ]
+    },
+    {
+        id: 37,
+        question: " In a certain code, COMPUTER is written as RFUVQNPC. How is MEDICINE written in the same code?",
+        option: [
+            'EOJDJEFM',
+            'EOJDEJFM',
+            'MFEJDJOE',
+            'MFEDJJOE'
+        ]
+    },
+    {
+        id: 38,
+        question: "If ROSE is coded as 6821, CHAIR is coded as 73456 and PREACH is coded as 961473, what will be the code for SEARCH? ",
+        option: [
+            '246173',
+            '214673',
+            '214763',
+            '216473'
+        ]
+    },
+    {
+        id: 39,
+        question: "Last year, there were 3 sections in the Catalyst, a mock CAT paper. Out of them 33 students cleared the cut-off in Section 1, 34 students cleared the cut-off in Section 2 and 32 cleared the cut-off in Section 3. 10 students cleared the cut-off in Section 1 and Section 2, 9 cleared the cut-off in Section 2 and Section 3, 8 cleared the cut-off in Section 1 and Section 3. The number of people who cleared each section alone was equal and was 21 for each section. How many cleared only one of the three sections? ",
+        option: [
+            '33',
+            '35',
+            '63',
+            '74'
+        ]
+    },
+    {
+        id: 40,
+        question: "There are 20000 people living in Defence Colony, Gurgaon. Out of them 9000 subscribe to Star TV Network and 12000 to Zee TV Network. If 4000 subscribe to both, how many do not subscribe to any of the two?",
+        option: [
+            '5000',
+            '2000',
+            '3000',
+            '1000'
+        ]
+    },
+    {
+        id: 41,
+        question: "In how many ways can the letters of the word 'LEADER' be arranged?",
+        option: [
+            '72',
+            '144',
+            '360',
+            '720'
+        ]
+    },
+    {
+        id: 42,
+        question: " In how many different ways can the letters of the word 'CORPORATION' be arranged so that the vowels always come together?",
+        option: [
+            '810',
+            '1440',
+            '2880',
+            '50400'
+        ]
+    },
+    {
+        id: 43,
+        question: "In how many ways can 8 Indians and, 4 American and 4 Englishmen can be seated in a row so that all person of the same nationality sit together? ",
+        option: [
+            '3! 4! 8! 4!',
+            '3! 8!',
+            '4! 4! ',
+            '8! 4! 4! '
+        ]
+    },
+    {
+        id: 44,
+        question: "In how many ways 4 boys and 3 girls can be seated in a row so that they are alternate",
+        option: [
+            '144',
+            '288',
+            '12',
+            '256'
+        ]
+    },
+    {
+        id: 45,
+        question: "What was the day on 15th august 1947? ",
+        option: [
+            'Friday',
+            'Saturday',
+            'Sunday',
+            'Thursday'
+        ]
+    },
+    {
+        id: 46,
+        question: "Which two months in a year have the same calendar? ",
+        option: [
+            'October, December ',
+            'April, November',
+            'June, October',
+            'April, July '
+        ]
+    },
+    {
+        id: 47,
+        question: "How many leap years do 300 years have? ",
+        option: [
+            '75 ',
+            '74',
+            '72',
+            '73 '
+        ]
+    },
+    {
+        id: 48,
+        question: "Nicholas said, “This girl is the wife of the grandson of my mother.” Who is Nicholas to this girl? ",
+        option: [
+            'Father ',
+            'Grandfather',
+            'Husband',
+            'Father-in-Law '
+        ]
+    },
+    {
+        id: 49,
+        question: "A is the brother of B and C is the sister of D. While Q is the daughter of B, P is the brother of Q. Determine A’s cousins.",
+        option: [
+            '(Q and P) ',
+            '(C and P)',
+            '(A and P)',
+            '(B and C) '
+        ]
+    },
+    {
+        id: 50,
+        question: "Deepak said to Raj, “The boy playing with football is the younger of the two brothers of the daughter of my father’s wife.” How is the boy playing football related to Deepak? ",
+        option: [
+            'Cousin',
+            'Son',
+            'Brother',
+            'Brother-In-Law'
+        ]
+    },
+    
+]
+
+
+
+export const answers = [1,3,3,1,3,2,3,2,3,2,2,2,0,1,0,2,2,2,3,3,1,0,2,0,2,2,3,3,2,1,3,2,1,2,0,0,0,2,2,2,2,3,0, 0, 0, 3,2,2,0,1];
